b6a04d19446ce13a9967d20724ae9f5dac3cc1fb
Amaury Rodriguez <amauryrlm@gmail.com>
2022-01-11 17:29:30 +0100

    commentaire

--- programme/main_sim_car.m
+++ programme/main_sim_car.m
@@ -14,14 +14,14 @@ intersect_int=[];
     %xt1=[200 200 200 500 500 300 300 500 200]
     %yt1=[200 350 650 700 600 500 300 200 200]
     
-    xt = [-130 -110 40 100 100 -30 -130]; %mapshow relis les points deux à deux, il faut terminer par le premier pour bien fermer le circuit
-    yt = [-50 10 90 0 -110 -130 -50];
+    x_circuit_ext = [-130 -110 40 100 100 -30 -130]; %mapshow relis les points deux à deux, il faut terminer par le premier pour bien fermer le circuit
+    y_circuit_ext = [-50 10 90 0 -110 -130 -50];
 
-    xt1 =[-180 -160 40 150 150 -80 -180];
-    yt1 = [-50 10 140 0 -160 -180 -50];
-    mapshow(xt,yt,'Marker','*')
-    mapshow(xt1,yt1,'Marker','+')
-    pas_teta=pi/10;
+    x_circuit_int =[-180 -160 40 150 150 -80 -180];
+    y_circuit_int = [-50 10 140 0 -160 -180 -50];
+    mapshow(x_circuit_ext,y_circuit_ext,'Marker','*')
+    mapshow(x_circuit_int,y_circuit_int,'Marker','+')
+
 
     setenv("delta_f","0");%angle de braquage initiale
 
@@ -34,7 +34,7 @@ xdotinit=0; % en m/s
 ydotinit=10; % en m/s
 psidotinit=0; % en rad/s
 
-XINIT=[xinit;yinit;psiinit;xdotinit;ydotinit;psidotinit];
+X=[xinit;yinit;psiinit;xdotinit;ydotinit;psidotinit];
 
 T0=0; % temps initial
 while T0<duree_simulation %simulation pour <-- secondes
@@ -45,14 +45,15 @@ while T0<duree_simulation %simulation pour <-- secondes
 
 
     TFINAL=T0+frequence_des_scannes; % pas temporel des scan
-    [TOUT,XOUT]=ode45('model_car',[T0 TFINAL],XINIT);
+    [TOUT,XOUT]=ode45('model_car',[T0 TFINAL],X);
 
-%rayons
 
-    
-    r=50;
-    
-    [x_finint,y_finint,x_finext,y_finext]=lidar(pas_teta,r,XINIT,xt,yt,xt1,yt1)
+%fonction lidar 
+
+pas_teta=pi/10;
+portee_lidar=50;
+
+[x_finint,y_finint,x_finext,y_finext]=lidar(pas_teta,portee_lidar,X,x_circuit_ext,y_circuit_ext,x_circuit_int,y_circuit_int)
     
 
     %tracage des point detecté et calculs des moyens de distances
@@ -61,7 +62,7 @@ while T0<duree_simulation %simulation pour <-- secondes
         mapshow(x_finint,y_finint,'DisplayType','point','Marker','o');
         moyint=0;
     for i=1:size(x_finint,2)
-        moyint=moyint+calcul_distance([x_finint(i),XINIT(1)],[y_finint(i),XINIT(2)]);
+        moyint=moyint+calcul_distance([x_finint(i),X(1)],[y_finint(i),X(2)]);
         moyint=moyint/size(x_finint,2);
     end
     end
@@ -69,11 +70,11 @@ while T0<duree_simulation %simulation pour <-- secondes
         mapshow(x_finext,y_finext,'DisplayType','point','Marker','o');
         moyext=0;
     for i=1:size(x_finext,2)
-        moyext=moyext+calcul_distance([x_finext(i),XINIT(1)],[y_finext(i),XINIT(2)]);
+        moyext=moyext+calcul_distance([x_finext(i),X(1)],[y_finext(i),X(2)]);
         moyext=moyext/size(x_finext,2);
     end
     else
-        moyint=r*10;
+        moyint=portee_lidar*10;
     end
     %changement d'angle de braquage
     intersect_ext=[intersect_ext moyext];
@@ -108,7 +109,7 @@ for i=1:pas_de_tracage_figure:size(TOUT)
         pause(0.1);
 end
 long=size(TOUT);
-XINIT=[XOUT(long(1),1);XOUT(long(1),2);XOUT(long(1),3);XOUT(long(1),4);XOUT(long(1),5);XOUT(long(1),6)];
+X=[XOUT(long(1),1);XOUT(long(1),2);XOUT(long(1),3);XOUT(long(1),4);XOUT(long(1),5);XOUT(long(1),6)];
 T0=TFINAL; % temps initial reset
 end
 
